e1db9e64fc4b600d260284e3ce29129c5e679cb4
bkalika <bogdan.kalika@gmail.com>
2020-06-21 10:29:09 +0300

    add the register/edit account feature

--- bookmarks/account/admin.py
+++ bookmarks/account/admin.py
@@ -1,3 +1,8 @@
+from .models import Profile
+
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_of_birth', 'photo']
--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('', views.dashboard, name='dashboard'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
     # # change password urls
     # path('password_change/', auth_views.PasswordChangeView.as_view(),
     #      name='password_change'),
@@ -18,6 +18,10 @@ urlpatterns = [
     # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     # path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    ### instead of above urls we can just write:###
+
+    # ---Instead of above urls we just can write:---
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    # Edit the user profile
+    path('edit/', views.edit, name='edit'),
 ]
