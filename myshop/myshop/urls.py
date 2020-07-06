948e21a6af6e6d1ecf3c234904a4cdb46e29cc1f
bkalika <bogdan.kalika@gmail.com>
2020-07-06 23:15:50 +0300

    add orders

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -21,7 +21,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
