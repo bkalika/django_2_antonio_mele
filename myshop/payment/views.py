73943d0fb48cb33b1bd58b4d95f3f0a5ced8e205
bkalika <bogdan.kalika@gmail.com>
2020-07-19 17:49:12 +0300

    add PDF invoice

--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -1,5 +1,11 @@
+from io import BytesIO
+
 import braintree
+import weasyprint
+from django.conf import settings
+from django.core.mail import EmailMessage
 from django.shortcuts import render, get_object_or_404, redirect
+from django.template.loader import render_to_string
 
 from orders.models import Order
 
@@ -25,6 +31,19 @@ def payment_process(request):
             # store the unique transaction id
             order.braintree_id = result.transaction.id
             order.save()
+            # create invoice e-mail
+            subject = f'My Shop - Invoice no. {order.id}'
+            message = 'Please, find attached the invoice for your recent purchase.'
+            email = EmailMessage(subject, message, 'admin@myshop.com', [order.email])
+            # generate PDF
+            html = render_to_string('orders/order/pdf.html', {'order': order})
+            out = BytesIO()
+            stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+            weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
+            # attach PDF file
+            email.attach(f'order_{order.id}', out.getvalue(), 'application/pdf')
+            # send e-mail
+            email.send()
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
