00acd326323e3501ea3db16e225347a47613e3d1
bkalika <bogdan.kalika@gmail.com>
2020-07-19 19:27:07 +0300

    add coupon app

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.http import require_POST
 from shop.models import Product
 from .forms import CartAddProductForm
 from .cart import Cart
+from coupons.forms import CouponApplyForm
 
 
 @require_POST
@@ -33,4 +34,6 @@ def cart_detail(request):
             initial={'quantity': item['quantity'],
                      'update': True}
         )
-    return render(request, 'cart/detail.html', {'cart': cart})
+    coupon_apply_form = CouponApplyForm()
+
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
