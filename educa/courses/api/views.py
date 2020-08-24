eb8a18cbeb5d84fc615386815db5f2a6e445f1c7
Bohdan <bogdan.kalika@gmail.com>
2020-08-24 18:23:40 +0300

    further development an API

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,7 +1,14 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import action
 
-from .serializers import SubjectSerializer
-from ..models import Subject
+from .permissions import IsEnrolled
+from .serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
+from ..models import Subject, Course
 
 
 class SubjectListView(generics.ListAPIView):
@@ -12,3 +19,25 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    @action(methods=['post'],
+            detail=True,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+
+    @action(methods=['get'],
+            detail=True,
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
